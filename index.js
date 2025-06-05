c121d6068fb4b41a1010b6d6dfe51f6be6041af0
v_v_moon <vladimir.mun.47@gmail.com>
2025-06-06 00:27:37 +0500

    modules

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from: "USD", to: "EUR", amount: 100 })
+const os = require('os')
+let res = os.platform()
+console.log(res)
 
-currencyConverter.convert().then((response) => {
-    console.log(`Converted Amount: ${response}`)
-})
-
+const my_math = require('./my_math')
+let res1 = my_math.add(4, 5)
+let res2 = my_math.minus(4, 5)
+console.log(`Res1: ${res1}, Res2: ${res2}`)
